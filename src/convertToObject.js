b8fe1de37ac79ed257550374ec536ea91bd5226a
Oleksandr Vannovskyi <alexandervannovskiy@gmail.com>
2025-07-21 17:50:37 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,29 @@
 'use strict';
 
+const complexStylesString = require('./stylesString');
+
 /**
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceArr = sourceString
+    .split(';')
+    .map((str) => str.trim())
+    .filter((str) => str !== '');
+
+  const sourceObj = {};
+
+  sourceArr.forEach((item) => {
+    const [key, value] = item.split(':');
+
+    sourceObj[key.trim()] = value.trim();
+  });
+
+  return sourceObj;
 }
+
+convertToObject(complexStylesString);
 
 module.exports = convertToObject;
